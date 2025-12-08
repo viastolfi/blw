7e942178a3c7c13159955bf69bd3a23687cb9141
viastolfi <vincent.astolfi12@gmail.com>
2025-12-08 11:37:13 +0100

    pumb raygui into project

--- src/main.c
+++ src/main.c
@@ -1,18 +1,35 @@
 #include "raylib.h"
 
-int main(void)
+#define RAYGUI_IMPLEMENTATION
+#include "thirdparty/raygui.h"
+
+int main()
 {
-    InitWindow(800, 450, "raylib [core] example - basic window");
+    InitWindow(400, 200, "raygui - controls test suite");
+    SetTargetFPS(60);
+
+    bool showMessageBox = false;
 
     while (!WindowShouldClose())
     {
+        // Draw
+        //----------------------------------------------------------------------------------
         BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
+
+            if (GuiButton((Rectangle){ 24, 24, 120, 30 }, "#191#Show Message")) showMessageBox = true;
+
+            if (showMessageBox)
+            {
+                int result = GuiMessageBox((Rectangle){ 85, 70, 250, 100 },
+                    "#191#Message Box", "Hi! This is a message!", "Nice;Cool");
+
+                if (result >= 0) showMessageBox = false;
+            }
+
         EndDrawing();
     }
 
     CloseWindow();
-
     return 0;
 }
