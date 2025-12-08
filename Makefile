d2a08905ba9ad6d88f5c6d040277456cec708008
viastolfi <vincent.astolfi12@gmail.com>
2025-12-08 11:34:18 +0100

    pumb raylib into project

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 SRC = src
 BUILD = build
+RAYLIB_FOLDER = raylib-5.5_linux_amd64
 
 CS = \
 		 $(SRC)/main.c
@@ -15,12 +16,12 @@ CFLAGS = -Wall -Wextra -g
 all: $(BUILD)/blw
 
 $(BUILD)/blw: $(OBJ)
-	$(CC) -o $@ $^
+	$(CC) -o $@ $^ -L$(RAYLIB_FOLDER)/lib -l:libraylib.a -lm
 	@$(BUILD)/blw
 
 $(BUILD)/%.o: $(SRC)/%.c
 	@mkdir -p $(BUILD)
-	$(CC) $(CFLAGS) -c $< -o $@
+	$(CC) $(CFLAGS) -c $< -o $@ -I$(RAYLIB_FOLDER)/include
 
 clean:
-	rm -rm $(BUILD)
+	rm -rf $(BUILD)
--- src/main.c
+++ src/main.c
@@ -1,7 +1,18 @@
-#include <stdio.h>
+#include "raylib.h"
 
-int main(void) 
+int main(void)
 {
-  printf("Hello World!\n");  
-  return 0;
+    InitWindow(800, 450, "raylib [core] example - basic window");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
 }
