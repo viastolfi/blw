904bcfa41ed705815f96ea9eb8cc4eebe94f9bb4
viastolfi <vincent.astolfi12@gmail.com>
2025-12-08 11:47:17 +0100

    feat: raygui warner window created

--- src/main.c
+++ src/main.c
@@ -5,31 +5,22 @@
 
 int main()
 {
-    InitWindow(400, 200, "raygui - controls test suite");
-    SetTargetFPS(60);
+  InitWindow(500, 300, "BATTERY LEVEL WARNER");
+  SetTargetFPS(60);
+  bool leave = false;
 
-    bool showMessageBox = false;
+  while (!WindowShouldClose() && !leave)
+  {
+    BeginDrawing();
+    ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
 
-    while (!WindowShouldClose())
-    {
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-            ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
+    DrawText("You battery level is bellow 20% !", 80, 100, 20, LIGHTGRAY);
 
-            if (GuiButton((Rectangle){ 24, 24, 120, 30 }, "#191#Show Message")) showMessageBox = true;
+    if (GuiButton((Rectangle){ 0, 200, 500, 100}, "#191#Understand !")) leave = true;
 
-            if (showMessageBox)
-            {
-                int result = GuiMessageBox((Rectangle){ 85, 70, 250, 100 },
-                    "#191#Message Box", "Hi! This is a message!", "Nice;Cool");
+    EndDrawing();
+  }
 
-                if (result >= 0) showMessageBox = false;
-            }
-
-        EndDrawing();
-    }
-
-    CloseWindow();
-    return 0;
+  CloseWindow();
+  return 0;
 }
